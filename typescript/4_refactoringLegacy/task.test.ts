481fe3c4e0eddb3507b05d30ae1e03d28ddbadfa
Wayne Cassagne-Covell <wayne@motorway.co.uk>
2025-07-04 14:21:17 +0100

    test: discount code test refactor

--- typescript/4_refactoringLegacy/task.test.ts
+++ typescript/4_refactoringLegacy/task.test.ts
@@ -1,57 +1,54 @@
-import { DiscountCodes, OrderProcessor } from "./legacy";
+import {DiscountCodes, OrderProcessor} from "./legacy";
 
 describe("OrderProcessor", () => {
-  let target: OrderProcessor;
+    let target: OrderProcessor;
 
-  beforeEach(() => {
-    target = new OrderProcessor();
-  });
-  test("processes an order", () => {
-    const response = target.process([
-      { product_name: "test product", price: 20 },
-    ]);
+    beforeEach(() => {
+        target = new OrderProcessor();
+    });
+    test("processes an order", () => {
+        const response = target.process([
+            {product_name: "test product", price: 20},
+        ]);
 
-    expect(response.t).toBeDefined();
-    expect(response.u).toBeDefined();
-  });
+        expect(response.t).toBeDefined();
+        expect(response.u).toBeDefined();
+    });
 
-  test("processes multiple item orders without discount", () => {
-    const response = target.process([
-      { product_name: "test product", price: 20 },
-      { product_name: "another product", price: 5 },
-    ]);
+    test("processes multiple item orders without discount", () => {
+        const response = target.process([
+            {product_name: "test product", price: 20},
+            {product_name: "another product", price: 5},
+        ]);
 
-    expect(response.t).toBe(25);
-    expect(response.u).toBe(2);
-  });
+        expect(response.t).toBe(25);
+        expect(response.u).toBe(2);
+    });
 
-  test("processes an order with a welcome discount code", () => {
-    const response = target.process([
-      { product_name: "a product", price: 10 },
-      { product_name: "a product", price: 20 }
-    ], DiscountCodes.WELCOME10);
 
-    expect(response.t).toBe(27);
-    expect(response.u).toBe(2);
-  })
+    test.each([
+        {input: DiscountCodes.WELCOME10, expected: {amount: 27, number: 2}},
+        {input: DiscountCodes.NEWYEAR15, expected: {amount: 25.5, number: 2}},
+        {input: DiscountCodes.BLACKFRIDAY30, expected: {amount: 21, number: 2}},
+    ])('calculates with discount code $input', ({input, expected}) => {
 
-  test("applies 30% discount to items with BLACKFRIDAY30", () => {
-    const response = target.process([
-      { product_name: "product 1", price: 10 },
-      { product_name: "product 2", price: 20 }
-    ], DiscountCodes.BLACKFRIDAY30);
+        const response = target.process([
+            {product_name: "product 1", price: 10},
+            {product_name: "product 2", price: 20}
+        ], input);
 
-    expect(response.t).toBe(21);
-    expect(response.u).toBe(2);
-  })
+        expect(response.t).toBe(expected.amount)
+        expect(response.u).toBe(expected.number);
 
-  test("applies 15% discount to items with NEWYEAR15", () => {
-    const response = target.process([
-      { product_name: "product 1", price: 10 },
-      { product_name: "product 2", price: 20 }
-    ], DiscountCodes.NEWYEAR15);
+    });
 
-    expect(response.t).toBe(25.5);
-    expect(response.u).toBe(2);
-  });
+
+    test("applies 15% discount to single item with NEWYEAR15", () => {
+        const response = target.process([
+            {product_name: "product expensive", price: 200}
+        ], DiscountCodes.NEWYEAR15);
+
+        expect(response.t).toBe(170);
+        expect(response.u).toBe(1);
+    });
 });
