acd6e1b103793fd6698783121721fabe913ac7b3
Kizito Akhilome <kizito@motorway.co.uk>
2025-07-04 13:57:12 +0100

    chore: process multiple orders

--- typescript/4_refactoringLegacy/task.test.ts
+++ typescript/4_refactoringLegacy/task.test.ts
@@ -4,15 +4,24 @@ describe("OrderProcessor", () => {
   let target: OrderProcessor;
 
   beforeEach(() => {
-    target = new OrderProcessor()
-  })
+    target = new OrderProcessor();
+  });
   test("processes an order", () => {
-     const response = target.process([{
-        product_name: 'test product',
-        price: 20
-     }]);
+    const response = target.process([
+      { product_name: "test product", price: 20 },
+    ]);
 
-     expect(response.t).toBeDefined()
-     expect(response.u).toBeDefined()
+    expect(response.t).toBeDefined();
+    expect(response.u).toBeDefined();
+  });
+
+  test("processes multiple item orders without discount", () => {
+    const response = target.process([
+      { product_name: "test product", price: 20 },
+      { product_name: "another product", price: 5 },
+    ]);
+
+    expect(response.t).toBe(25);
+    expect(response.u).toBe(2);
   });
 });
