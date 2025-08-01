f6eb519c1e16fc454dce80274f16c166eca2959f
Wayne Cassagne-Covell <wayne@motorway.co.uk>
2025-08-01 11:27:39 +0100

    chore: rover moves in directions

--- typescript/5_marsRover/task.ts
+++ typescript/5_marsRover/task.ts
@@ -2,28 +2,38 @@ type Direction = "N" | "E" | "S" | "W";
 type Instruction = "M" | "L" | "R";
 
 const directionMap = {
-        N: 1,
-        E: 0,
-        S: 1,
-        W: 0
-    }
+    N: 1,
+    E: 0,
+    S: 1,
+    W: 0
+}
+
+const instructionMap = {
+    N: 1,
+    E: 1,
+    S: -1,
+    W: -1
+}
+
 export class Rover {
     constructor(
         private coordinates: [number, number],
         private direction: Direction
-    ) {}
-
-
+    ) {
+    }
 
     get position() {
         return {coords: this.coordinates, direction: this.direction}
     }
 
     instruct(instructions: Instruction[]) {
-        for(const instruction of instructions) {
-            switch(instruction) {
+        for (const instruction of instructions) {
+            switch (instruction) {
                 case "M":
-                    this.coordinates[directionMap[this.direction]] += 1
+                    const moveDirection = directionMap[this.direction];
+                    const amountToMove = instructionMap[this.direction];
+
+                    this.coordinates[moveDirection] += amountToMove;
 
                     break;
                 default:
