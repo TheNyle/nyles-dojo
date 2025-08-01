dcdea1461b34d1d0cede80a3b84a5bdf0df3168c
Kizito Akhilome <kizito@motorway.co.uk>
2025-08-01 12:00:32 +0100

    chore: refactor & complete instruction set

--- typescript/5_marsRover/task.test.ts
+++ typescript/5_marsRover/task.test.ts
@@ -1,47 +1,58 @@
-import {Rover} from "./task";
+import { Rover } from "./task";
 
 describe("Rover", () => {
-    it("has a rover", () => {
-        expect(Rover).toBeDefined();
-    });
+  it("has a rover", () => {
+    expect(Rover).toBeDefined();
+  });
 
-    it("has a position and direction", () => {
-        const rover = new Rover([0, 0], "N");
+  it("has a position and direction", () => {
+    const rover = new Rover([0, 0], "N");
 
-        expect(rover.position).toEqual({coords: [0, 0], direction: "N"})
-    });
+    expect(rover.position).toEqual({ coords: [0, 0], direction: "N" });
+  });
 
-    it("gives rover move instruction and validates new position", () => {
-        const rover = new Rover([0, 0], "N");
+  it("gives rover move instruction and validates new position", () => {
+    const rover = new Rover([0, 0], "N");
 
-        expect(rover.position).toEqual({coords: [0, 0], direction: "N"})
+    expect(rover.position).toEqual({ coords: [0, 0], direction: "N" });
 
-        rover.instruct(['M'])
+    rover.instruct(["M"]);
 
-        expect(rover.position).toEqual({coords: [0, 1], direction: "N"})
-    })
+    expect(rover.position).toEqual({ coords: [0, 1], direction: "N" });
+  });
 
-    it("handles multiple M instructions", () => {
-      const rover = new Rover([0, 0], "N");
+  it("handles multiple M instructions", () => {
+    const rover = new Rover([0, 0], "N");
 
-      rover.instruct(['M','M','M'])
+    rover.instruct(["M", "M", "M"]);
 
-      expect(rover.position).toEqual({ coords:[0,3], direction: "N"})
-    })
-    
-    it('handles multiple M instructions for diff directions', () => {
-      const rover = new Rover([0,0], "E");
-      
-      rover.instruct(['M', 'M']);
-      
-      expect(rover.position).toEqual({ coords:[2,0], direction: "E"});
-    });
+    expect(rover.position).toEqual({ coords: [0, 3], direction: "N" });
+  });
 
-    it('handles multiple M instructions for diff directions', () => {
-      const rover = new Rover([2,0], "S");
-      
-      rover.instruct(['M', 'M', "M"]);
-      
-      expect(rover.position).toEqual({ coords:[2,-3], direction: "S"});
-    });
+  it("handles multiple M instructions for diff directions", () => {
+    const rover = new Rover([0, 0], "E");
+
+    rover.instruct(["M", "M"]);
+
+    expect(rover.position).toEqual({ coords: [2, 0], direction: "E" });
+  });
+
+  it("handles multiple M instructions for diff directions", () => {
+    const rover = new Rover([2, 0], "S");
+
+    rover.instruct(["M", "M", "M"]);
+
+    expect(rover.position).toEqual({ coords: [2, -3], direction: "S" });
+  });
+
+  it("turns left and updates direction", () => {
+    const rover = new Rover([0, 0], "N");
+
+    expect(rover.position).toEqual({ coords: [0, 0], direction: "N" });
+    rover.instruct(["L", "M", "M", "L", "M"]);
+
+    expect(rover.position).toEqual({ coords: [-2, -1], direction: "S" });
+    rover.instruct(["M"]);
+    expect(rover.position).toEqual({ coords: [-2, -2], direction: "S" });
+  });
 });
--- typescript/5_marsRover/task.ts
+++ typescript/5_marsRover/task.ts
@@ -1,46 +1,44 @@
 type Direction = "N" | "E" | "S" | "W";
 type Instruction = "M" | "L" | "R";
+type DirectionConfig = Record<
+  Direction,
+  { coord: number; move: number; leftTurn: Direction; rightTurn: Direction }
+>;
 
-const directionMap = {
-    N: 1,
-    E: 0,
-    S: 1,
-    W: 0
-}
-
-const instructionMap = {
-    N: 1,
-    E: 1,
-    S: -1,
-    W: -1
-}
+const directionConfig: DirectionConfig = {
+  N: { coord: 1, move: 1, leftTurn: "W", rightTurn: "E" },
+  E: { coord: 0, move: 1, leftTurn: "N", rightTurn: "S" },
+  S: { coord: 1, move: -1, leftTurn: "E", rightTurn: "W" },
+  W: { coord: 0, move: -1, leftTurn: "S", rightTurn: "N" },
+};
 
 export class Rover {
-    constructor(
-        private coordinates: [number, number],
-        private direction: Direction
-    ) {
+  constructor(
+    private coordinates: [number, number],
+    private direction: Direction
+  ) {}
+
+  get position() {
+    return { coords: this.coordinates, direction: this.direction };
+  }
+
+  instruct(instructions: Instruction[]) {
+    for (const instruction of instructions) {
+      switch (instruction) {
+        case "M":
+          const moveDirection = directionConfig[this.direction].coord;
+          const amountToMove = directionConfig[this.direction].move;
+
+          this.coordinates[moveDirection] += amountToMove;
+          break;
+        case "L":
+          this.direction = directionConfig[this.direction].leftTurn;
+          break;
+        case "R":
+          this.direction = directionConfig[this.direction].rightTurn;
+        default:
+          break;
+      }
     }
-
-    get position() {
-        return {coords: this.coordinates, direction: this.direction}
-    }
-
-    instruct(instructions: Instruction[]) {
-        for (const instruction of instructions) {
-            switch (instruction) {
-                case "M":
-                    const moveDirection = directionMap[this.direction];
-                    const amountToMove = instructionMap[this.direction];
-
-                    this.coordinates[moveDirection] += amountToMove;
-
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
+  }
 }
-
